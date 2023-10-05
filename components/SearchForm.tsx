12c11d1437c2a7177be5ae21f2bf7217046356a2
YaroslavBerezka <jarik11jarik@ukr.net>
2023-10-05 15:20:58 +0300

    sirst commit

---
+++ components/SearchForm.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { formUrlQuery } from '@/sanity/utils'
+import Image from 'next/image'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { Input } from './ui/input'
+
+const SearchForm = () => {
+	const [search, setSearch] = useState('')
+
+	const searchParams = useSearchParams()
+	const router = useRouter()
+	const pathname = usePathname()
+
+	useEffect(() => {
+		const delayDebounceFn = setTimeout(() => {
+			let newUrl = ''
+
+			if (search) {
+				newUrl = formUrlQuery({
+					params: searchParams.toString(),
+					key: 'query',
+					value: search,
+				})
+			} else {
+				newUrl = formUrlQuery({
+					params: searchParams.toString(),
+					keysToRemove: ['query'],
+				})
+			}
+
+			router.push(newUrl, { scroll: false })
+		}, 300)
+
+		return () => clearTimeout(delayDebounceFn)
+	}, [search])
+
+	return (
+		<form className='flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5'>
+			<label className='flex-center relative w-full max-w-3xl'>
+				<Image
+					src='/magnifying-glass.svg'
+					className='absolute left-8'
+					width={32}
+					height={32}
+					alt='Search icon'
+				/>
+
+				<Input
+					value={search}
+					onChange={e => setSearch(e.target.value)}
+					type='text'
+					placeholder='Search'
+					className='base-regular h-fit border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-800'
+				/>
+			</label>
+		</form>
+	)
+}
+
+export default SearchForm
